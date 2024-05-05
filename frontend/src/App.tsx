39d9b79281fa3dbe6b94aec9a5925d80d263955b
steven <msycomtw@gmail.com>
2024-05-05 14:54:47 +0800

    ⭐ feat: Implement index page by using @MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { Grid, Button} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <code>Python Learning Club</code>
+
+          <Grid container spacing={2} justifyContent="center" alignItems="center">
+            <Grid item>
+              <a href='https://level-scallop-cd8.notion.site/Python-d12c0389e5874dceb9273432b849cd59?pvs=4' target='_blank' rel='noreferrer'>
+                <Button 
+                  variant="contained" 
+                  sx={{
+                    fontWeight: "bold",
+                    backgroundColor: "#FFF", // 自定義背景顏色
+                    color: "#111", // 文字顏色
+                    '&:hover': {
+                      backgroundColor: "#FCFAB6", // 滑鼠懸停時的背景顏色
+                    }
+                  }}>
+                  近期活動
+                </Button>
+              </a>
+            </Grid>
+
+            <Grid item>
+              <a href='https://level-scallop-cd8.notion.site/Python-f88abbfaf6fc4caf9c0e61048eea53b9?pvs=4' target='_blank' rel='noreferrer'>
+                <Button 
+                variant="contained" 
+                sx={{
+                  fontWeight: "bold",
+                  backgroundColor: "#FFF", // 自定義背景顏色
+                  color: "#111", // 文字顏色
+                  '&:hover': {
+                    backgroundColor: "#FCFAB6", // 滑鼠懸停時的背景顏色
+                  }
+                }}>
+                學習資源
+                </Button>
+              </a>
+            </Grid>
+
+            <Grid item>
+              <a href='https://docs.google.com/forms/d/e/1FAIpQLScF3E9_u7Sldp050SJnu9liMkSwJ8-kA_gOVlqnpiqTdZ0FFw/viewform?usp=sf_link' target='_blank' rel='noreferrer'>
+                <Button 
+                variant="contained" 
+                sx={{
+                  fontWeight: "bold",
+                  backgroundColor: "#FFF", // 自定義背景顏色
+                  color: "#111", // 文字顏色
+                  '&:hover': {
+                    backgroundColor: "#FCFAB6", // 滑鼠懸停時的背景顏色
+                  }
+                }}>
+                學習計畫
+                </Button>
+              </a>
+            </Grid>
+          </Grid>
       </header>
     </div>
   );
